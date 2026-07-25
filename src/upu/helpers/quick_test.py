4d3a0da3b128456eeff29f2e6da2493c0c4b0bb5
GrCOTE7 <GrCOTE7@gmail.com>
2026-07-25 11:38:54 +0200

    better quick test

--- src/upu/helpers/quick_test.py
+++ src/upu/helpers/quick_test.py
@@ -2,12 +2,12 @@ from get_env import get_env
 import os
 
 print(get_env("UPU_WINDOW_LEFT", 555))
-print(os.environ.get("FLET_RUNNING"))
 
 if __name__ == "__main__":
 
+    # print('Flet is' , ('running' if os.environ.get("FLET_RUNNING") else 'not yet started'))
     if os.environ.get("FLET_RUNNING") != "1":
         import subprocess
 
-        env["FLET_RUNNING"] = "1"
+        os.environ["FLET_RUNNING"] = "1"
         subprocess.run(["flet", "run", "src/upu/helpers/quick_test.py"])
