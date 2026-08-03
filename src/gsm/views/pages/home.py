1628108847fb1720a3c938822072f0976f97c319
GrCOTE7 <GrCOTE7@gmail.com>
2026-08-03 10:18:00 +0200

    prepare UpU

--- src/gsm/views/pages/home.py
+++ src/gsm/views/pages/home.py
@@ -12,5 +12,6 @@ class HomePage:
             controls=[
                 ft.Text("Accueil", size=28, weight=ft.FontWeight.BOLD),
                 ft.Text("Squelette d'application Flet déclarative\n\n(Projet francophone collaboratif 'gsm' basé sur Python et Flet (declarative mode - React & Flutter-like)."),
+                ft.Text("GH : http://GitHub.com/GrCOTE7/GSM", weight=ft.FontWeight.BOLD),
             ],
         )
